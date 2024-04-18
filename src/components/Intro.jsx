0cfd0122aac8bc4e362139f520501c643cd76bae
kr-suraja <suraj.a@knackroot.com>
2024-04-18 17:29:26 +0530

    change 2

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -16,7 +16,7 @@ const Intro = () => {
                 <div className="xl:max-w-xl w-full">
                     <h1 className="text-3xl text-center xl:text-start font-patua  xl:text-[3rem] text-[#140346]  tracking-wide  font-intro leading-[3.5rem] " style={{ fontFamily: "Patua One" }} >Craft, Share, Earn </h1>
                     <div className="mt-8 text-base text-center xl:text-start xl:text-2xl  text-[#484848] font-medium px-4 xl:px-0 ">
-                        Unlock the power of Blockchain Supported Survey, Earn reward for your data!
+                         the power of Blockchain Supported Survey, Earn reward for your data!
                     </div>
                     <div className="flex justify-center xl:justify-start">
                         <Button className={"bg-[#5831C8] mt-10 w-[230px] font-poppins "} arrow={"right"}><div className="font-['Poppins'] " style={{ fontFamily: "Poppins" }}>Get Started</div></Button>
